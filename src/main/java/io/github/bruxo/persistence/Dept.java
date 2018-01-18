0a610b8af8053577d4a782830d2a8e15391b2f13
bruxo <bruxao@msn.com>
2018-01-18 10:57:52 -0200

    Changed Dept Entity to annotation

--- src/main/java/io/github/bruxo/persistence/Dept.java
+++ src/main/java/io/github/bruxo/persistence/Dept.java
@@ -1,13 +1,29 @@
 package io.github.bruxo.persistence;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="DEPT")
 public class Dept {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", length=11, nullable=false, unique=true)
 	private int id;
+	@Column(name = "name", length=20, nullable=true)
 	private String name;
+	@Column(name = "phone", length=20, nullable=true)
 	private String phone;
 	
 	public Dept () {
 		
 	}
+	
 	public int getId() {
 		return id;
 	}
